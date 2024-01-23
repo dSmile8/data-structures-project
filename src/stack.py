42007399b672c2e3871c9eb8785a612c8fa7771c
Сергей Арефьев <Simpson6515@yandex.ru>
2024-01-24 01:58:48 +0500

    realize classes Node and Stack, write test for stack

--- src/stack.py
+++ src/stack.py
@@ -4,10 +4,10 @@ class Node:
     def __init__(self, data, next_node):
         """
         Конструктор класса Node
-
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,15 +15,19 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
 
-    def push(self, data):
+        self.top = None
+
+    def push(self, data) -> None:
         """
         Метод для добавления элемента на вершину стека
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+
+        next_node = self.top
+        new_top = Node(data, next_node)
+        self.top = new_top
 
     def pop(self):
         """
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,26 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack, Node
+
+
+class TestStack(unittest.TestCase):
+    def test_push(self):
+
+        self.assertEqual(Stack().push('DaTa1'), None)
+
+        node = Node('DaTa1', 'DaTa2')
+        self.assertEqual(node.next_node, 'DaTa2')
+        self.assertEqual(node.data, 'DaTa1')
+
+        stack = Stack()
+
+        stack.push('111111')
+        self.assertEqual(stack.top.data, '111111')
+
+        stack.push('2222')
+        self.assertEqual(stack.top.data, '2222')
+        self.assertEqual(stack.top.next_node.data, '111111')
+
+
+if __name__ == '__main__':
+    unittest.main()
